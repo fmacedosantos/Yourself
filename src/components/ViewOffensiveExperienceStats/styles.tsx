b0f40d4466217f3b3984d9b2dd8cf2b80b7f79ee
fmacedosantos <felipemacedo986@gmail.com>
2024-08-22 11:05:39 -0300

    Estilizando o componente ViewOffensiveExperienceStats

---
+++ src/components/ViewOffensiveExperienceStats/styles.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../constants/colors';
+
+export const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        position: 'absolute',
+        top: 0,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        width: '100%',
+        paddingTop: 30,
+        paddingBottom: 10,
+        backgroundColor: COLORS.WHITE
+      },
+      statItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      statValue: {
+        marginLeft: 5,
+      }
+});
