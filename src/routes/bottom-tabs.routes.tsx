493114c953a474df4500ae79110680c825d5d8db
fmacedosantos <felipemacedo986@gmail.com>
2024-08-22 11:29:17 -0300

    Adicionando ícones e escondendo o header

--- src/routes/bottom-tabs.routes.tsx
+++ src/routes/bottom-tabs.routes.tsx
@@ -2,13 +2,30 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from "../screens/Home";
 import { Tasks } from "../screens/Tasks";
 
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { COLORS } from "../constants/colors";
+
 const {Navigator, Screen} = createBottomTabNavigator()
 
 export function BottomTabRoutes(){
     return(
         <Navigator>
-            <Screen name="Home" component={Home}/>
-            <Screen name="Tasks" component={Tasks}/>
+            <Screen name="Home" component={Home}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({color, size}) => (
+                        <FontAwesome5 name="home" size={24} color={COLORS.BLACK} />
+                    )
+                }}
+            />
+            <Screen name="Tasks" component={Tasks}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({color, size}) => (
+                        <FontAwesome5 name="tasks" size={24} color={COLORS.BLACK} />
+                    )
+                }}
+            />
         </Navigator>
     )
 }
