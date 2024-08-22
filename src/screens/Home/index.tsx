3716c09a8c9054238bd6c54d31c4035d8bfc5155
fmacedosantos <felipemacedo986@gmail.com>
2024-08-22 11:06:02 -0300

    Aplicando o componente na Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import { styles } from './styles';
 import { ButtonTask } from '../../components/ButtonTask';
-
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { ViewOffensiveExperienceStats } from '../../components/ViewOffensiveExperienceStats';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <View style={styles.containerDetails}>
-        <View style={styles.containerAchievements}>
-          <FontAwesome5 name="fire" size={24} color="orange" />
-          <Text style={styles.textAchievements}>368 dias</Text>
-        </View>
-
-        <View style={styles.containerAchievements}>
-          <FontAwesome5 name="certificate" size={24} color="black"/>
-          <Text style={styles.textAchievements}>2168 XP</Text>
-        </View>
-      </View>
+      <ViewOffensiveExperienceStats/>
       <ButtonTask/>
     </View>
   );
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -8,22 +8,4 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: COLORS.WINTER.BACKGROUND,
   },
-  containerDetails: {
-    flexDirection: 'row',
-    position: 'absolute',
-    top: 0,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    width: '100%',
-    paddingTop: 30,
-    paddingBottom: 10,
-    backgroundColor: COLORS.WHITE
-  },
-  containerAchievements: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  textAchievements: {
-    marginLeft: 5,
-  }
 });
