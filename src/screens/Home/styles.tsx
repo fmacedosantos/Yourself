108dd62780b9cb3597c61f5e948d30a250eefc1a
fmacedosantos <felipemacedo986@gmail.com>
2024-08-22 10:55:19 -0300

    Estilizando os detalhes de ofensiva e XP

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -8,4 +8,22 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: COLORS.WINTER.BACKGROUND,
   },
+  containerDetails: {
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 0,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '100%',
+    paddingTop: 30,
+    paddingBottom: 10,
+    backgroundColor: COLORS.WHITE
+  },
+  containerAchievements: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textAchievements: {
+    marginLeft: 5,
+  }
 });
