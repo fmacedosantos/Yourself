fa76a8a7567f55ffb80ae63281e1e269bdd8face
fmacedosantos <felipemacedo986@gmail.com>
2024-08-22 10:55:04 -0300

    Criando os detalhes de ofensiva e XP

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import { styles } from './styles';
 import { ButtonTask } from '../../components/ButtonTask';
 
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
 export function Home() {
   return (
     <View style={styles.container}>
+      <View style={styles.containerDetails}>
+        <View style={styles.containerAchievements}>
+          <FontAwesome5 name="fire" size={24} color="orange" />
+          <Text style={styles.textAchievements}>368 dias</Text>
+        </View>
+
+        <View style={styles.containerAchievements}>
+          <FontAwesome5 name="certificate" size={24} color="black"/>
+          <Text style={styles.textAchievements}>2168 XP</Text>
+        </View>
+      </View>
       <ButtonTask/>
     </View>
   );
