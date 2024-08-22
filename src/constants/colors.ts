b95898cc5cae6ce71deb50b7c2f7f2161849666e
fmacedosantos <felipemacedo986@gmail.com>
2024-08-22 10:20:41 -0300

    Definindo a constante da paleta do app

---
+++ src/constants/colors.ts
@@ -0,0 +1,19 @@
+export const COLORS = {
+    WHITE: '#FDFDFD',
+    BLACK: '#373435',
+    WINTER: {
+        BACKGROUND: '#DCEEF1',
+        ICON: {
+            GREEN: '#08805F',
+            RED: '#D15745',
+        },
+    },
+    SUMMER: {
+        BACKGROUND: '#FBB24A',
+        ICON: {
+            GREEN: '#7DBA2E',
+            RED: '#EB422F',
+        },
+    }
+    
+}
