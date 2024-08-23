59a51804b0eb3bdd767a4681d8dc36ce8f3b337a
fmacedosantos <felipemacedo986@gmail.com>
2024-08-23 11:42:40 -0300

    Recebendo ações para ButtonTask

--- src/components/ButtonTask/index.tsx
+++ src/components/ButtonTask/index.tsx
@@ -3,9 +3,10 @@ import { Text, TouchableOpacity, View } from 'react-native';
 
 import { styles } from './styles';
 
-export function ButtonTask() {
+export function ButtonTask({onPress}: any) {
   return (
-    <TouchableOpacity style={styles.buttonCreateTask}>
+    <TouchableOpacity style={styles.buttonCreateTask}
+    onPress={onPress}>
       <Text style={styles.textCreateTask}>+</Text>
     </TouchableOpacity>
   );
