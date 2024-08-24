767e0751a6b3feb6e4ae2ae7f3f7f40ae323085b
fmacedosantos <felipemacedo986@gmail.com>
2024-08-24 20:31:56 -0300

    Usando o navigate nos botões (excluindo os antigos arquivos de VIewCreateTask)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,22 +4,15 @@ import { View } from 'react-native';
 import { styles } from './styles';
 import { RoundedButtonCreateTask } from '../../components/RoundedButtonCreateTask';
 import { ViewSummaryStats } from '../../components/ViewSummaryStats';
-import { ViewCreateTask } from '../../components/ViewCreateTask';
 
-export function Home() {
-  const [isCreatingTask, setIsCreatingTask] = useState(false);
-
-  const handleCloseCreateTask = () => {
-    setIsCreatingTask(false);
-  };
+export function Home({navigation}: any) {
   return (
     <View style={styles.container}>
       <ViewSummaryStats/>
       <RoundedButtonCreateTask onPress={() => {
-          setIsCreatingTask(true)
+          navigation.navigate('tasks')
         }}/>
 
-        {isCreatingTask && (<ViewCreateTask onClose={handleCloseCreateTask}/>)}
     </View>
   );
 }
--- src/screens/Tasks/index.tsx
+++ src/screens/Tasks/index.tsx
@@ -1,29 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { Alert, Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { View } from 'react-native';
 
 import { styles } from './styles';
 
 import { ViewSummaryStats } from '../../components/ViewSummaryStats';
-import { RoundedButtonCreateTask } from '../../components/RoundedButtonCreateTask';
-import { ViewCreateTask } from '../../components/ViewCreateTask';
 import { ButtonCreateTask } from '../../components/ButtonCreateTask';
 
-export function Tasks() {
-  const [isCreatingTask, setIsCreatingTask] = useState(false);
-
-  const handleCloseCreateTask = () => {
-    setIsCreatingTask(false);
-  };
+export function Tasks({navigation}: any) {
 
   return (
     <View style={styles.container}>
         <ViewSummaryStats/>
         
         <ButtonCreateTask onPress={() => {
-          setIsCreatingTask(true)
+          navigation.navigate('CreateTask')
         }}/>
 
-        {isCreatingTask && (<ViewCreateTask onClose={handleCloseCreateTask}/>)}
     </View>
   );
 }
