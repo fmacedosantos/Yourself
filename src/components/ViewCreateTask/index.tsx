2de20aeb426b7e1511a2fd94d0313365ea68f0f9
fmacedosantos <felipemacedo986@gmail.com>
2024-08-24 17:13:53 -0300

    Retirando importações desnecessárias e mudando o título na ViewCreateTask

--- src/components/ViewCreateTask/index.tsx
+++ src/components/ViewCreateTask/index.tsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { styles } from './styles';
-import icon from '../../../assets/pictures/icon.png'
-import { COLORS } from '../../constants/colors';
 import { TextInputPaper } from '../TextInputPaper';
 import { SelectDifficulty } from '../SelectDifficulty';
 
@@ -12,7 +10,7 @@ export function ViewCreateTask({onClose}: any) {
   return (
     <View style={styles.container}>
       <Text onPress={onClose} style={styles.closeView}>Sair</Text>
-        <Text style={styles.title}>INICIANDO TAREFA</Text>
+        <Text style={styles.title}>TAREFA</Text>
         <TextInputPaper label='Título' placeholder='Titule sua tarefa'/>
 
         <TextInputPaper label='Descrição' placeholder='Descreva sua tarefa' 
