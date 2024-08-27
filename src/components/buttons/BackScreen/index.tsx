108e09674bcf9b3d28e22c43deb808cd6ebe2f5a
fmacedosantos <felipemacedo986@gmail.com>
2024-08-27 10:09:04 -0300

    Permitindo estilização em BackScreen

--- src/components/buttons/BackScreen/index.tsx
+++ src/components/buttons/BackScreen/index.tsx
@@ -3,12 +3,17 @@ import { Text, View } from 'react-native';
 
 import { styles } from './styles';
 
-export function BackScreen({ navigation }: any) {
+interface backScreenProps{
+  navigation: any
+  style?: any
+}
+
+export function BackScreen({ navigation, style }: backScreenProps) {
     const goBack = () => {
         navigation.goBack()
       }
 
   return (
-    <Text onPress={goBack} style={styles.closeView}>Sair</Text>
+    <Text onPress={goBack} style={[styles.closeView, style]}>Sair</Text>
   );
 }
