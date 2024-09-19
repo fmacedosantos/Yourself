6c8001f6c3cd86a02d1cd5123c1075793b53da52
fmacedosantos <felipemacedo986@gmail.com>
2024-09-19 14:59:41 -0300

    Atualizando os ícones do menu

--- src/routes/bottom-tabs.routes.tsx
+++ src/routes/bottom-tabs.routes.tsx
@@ -2,41 +2,47 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from "../screens/Home/HomeScreen";
 import { Tasks } from "../screens/NewActivity/NewActivityScreen";
 
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import { COLORS } from "../constants/colors";
+import historicalIcon from '../../assets/icons/historical-icon.svg';
+import addActivityIcon from '../../assets/icons/add_activity-icon.svg';
+import shopIcon from '../../assets/icons/shop-icon.svg';
+import profileIcon from '../../assets/icons/profile-icon.svg';
 import { Shop } from "../screens/Shop/ShopScreen";
 import { Profile } from "../screens/Profile/ProfileScreen";
+import { Image } from "react-native";
 
 const Tab = createBottomTabNavigator()
 
 export function BottomTabRoutes(){
     return(
-        <Tab.Navigator screenOptions={{headerShown: false, tabBarShowLabel: false}}>
+        <Tab.Navigator screenOptions={{
+            headerShown: false, 
+            tabBarShowLabel: false
+            }}>
             <Tab.Screen name="home" component={Home}
                 options={{
-                    tabBarIcon: ({color, size}) => (
-                        <FontAwesome5 name="home" size={27} color={COLORS.BLACK} />
+                    tabBarIcon: () => (
+                        <Image source={historicalIcon}/>
                     )
                 }}
             />
             <Tab.Screen name="tasks" component={Tasks}
                 options={{
-                    tabBarIcon: ({color, size}) => (
-                        <FontAwesome5 name="tasks" size={27} color={COLORS.BLACK} />
+                    tabBarIcon: () => (
+                        <Image source={addActivityIcon}/>
                     )
                 }}
             />
             <Tab.Screen name="shop" component={Shop}
                 options={{
-                    tabBarIcon: ({color, size}) => (
-                        <FontAwesome5 name="shopping-cart" size={27} color="black" />
+                    tabBarIcon: () => (
+                        <Image source={shopIcon}/>
                     )
                 }}
             />
             <Tab.Screen name="profile" component={Profile}
                 options={{
-                    tabBarIcon: ({color, size}) => (
-                        <FontAwesome5 name="male" size={27} color="black" />
+                    tabBarIcon: () => (
+                        <Image source={profileIcon}/>
                     )
                 }}
             />
