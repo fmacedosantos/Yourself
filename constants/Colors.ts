c258ddddcad205fcabb0a55ec409add69c8eada9
fmacedosantos <felipemacedo986@gmail.com>
2024-09-23 14:16:21 -0300

    Atualizando a paleta do projeto

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,11 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
-export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-};
+export const COLORS = {
+  WHITE: '#FDFDFD',
+  GRAY: '#373435',
+  BLACK: '#000000',
+  RED: '#EB422F',
+  GREEN: '#7DBA2E',
+  BLUE: '#4AD1FB',
+  DARK_BLUE: '#4A91FB',
+  ORANGE: '#FFB636',
+  DARK_ORANGE: '#FF8F1F',
+}
