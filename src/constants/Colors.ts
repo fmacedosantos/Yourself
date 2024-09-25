daa5d31273f1f6f6190fbe73bd6ccaa8d2824859
fmacedosantos <felipemacedo986@gmail.com>
2024-09-24 23:00:25 -0300

    Criando as constantes que envolvem a paleta de cores

---
+++ src/constants/Colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+    WHITE: '#FDFDFD',
+    GRAY: '#373435',
+    BLACK: '#000000',
+    RED: '#EB422F',
+    GREEN: '#7DBA2E',
+    BLUE: '#4AD1FB',
+    DARK_BLUE: '#4A91FB',
+    ORANGE: '#FFB636',
+    DARK_ORANGE: '#FF8F1F',
+}
