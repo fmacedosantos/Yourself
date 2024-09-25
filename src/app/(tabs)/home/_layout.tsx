61611376230e3a9db16d5b4b84c43b9481135e2b
fmacedosantos <felipemacedo986@gmail.com>
2024-09-24 23:16:31 -0300

    Adicionando os ícones

--- src/app/(tabs)/home/_layout.tsx
+++ src/app/(tabs)/home/_layout.tsx
@@ -1,17 +1,18 @@
 import { Tabs } from "expo-router";
-import { FontAwesome } from '@expo/vector-icons'
+import {FontAwesome} from '@expo/vector-icons'
 
 export default function Layout() {
   return (
     <Tabs>
         <Tabs.Screen name="index" options={{
           headerShown: false, 
+          tabBarShowLabel: false,
           tabBarIcon: ({focused, color, size}) => {
 
             if(focused){
-              return <FontAwesome name="home" color="red" size={size}/>
+              return <FontAwesome name="home" size={size} color={color}/>
             }
-            return <FontAwesome name="home" color={color} size={size}/>
+            return <FontAwesome name="home" size={size} color={color}/>
           }
           }}/>
     </Tabs>
