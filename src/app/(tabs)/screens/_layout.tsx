26742c3ade3a4504c6fdfac5123544b6ffebe9a5
fmacedosantos <felipemacedo986@gmail.com>
2024-09-25 11:36:36 -0300

    Configurando svgs

--- src/app/(tabs)/screens/_layout.tsx
+++ src/app/(tabs)/screens/_layout.tsx
@@ -1,20 +1,23 @@
 import { Tabs } from "expo-router";
-import {FontAwesome} from '@expo/vector-icons'
+import HistoricalIcon from "../../../assets/icons/historical-icon.svg"; // Importe o SVG como componente
 
 export default function Layout() {
   return (
     <Tabs>
-        <Tabs.Screen name="index" options={{
-          headerShown: false, 
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
           tabBarShowLabel: false,
-          tabBarIcon: ({focused, color, size}) => {
-
-            if(focused){
-              return <FontAwesome name="home" size={size} color={color}/>
-            }
-            return <FontAwesome name="home" size={size} color={color}/>
-          }
-          }}/>
+          tabBarIcon: ({ focused, color, size }) => (
+            <HistoricalIcon
+              width={35}
+              height={35}
+              fill={'white'} 
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
