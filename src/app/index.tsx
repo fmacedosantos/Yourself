2f12aac641d465277958caea70884f13d56d3b88
fmacedosantos <felipemacedo986@gmail.com>
2024-10-23 23:24:59 -0300

    Logando com os valores digitados

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,56 +1,71 @@
+import { useState } from "react";
 import { router } from "expo-router";
-import { Text, View, Button, StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { BigButton } from "../components/bigButton";
 import { COLORS } from "../constants/Colors";
-import FormInput from "../components/formInput";
+import { FormInput } from "../components/formInput";
 import { TextIcon } from "../components/textIcon";
 
 import firebase from '../../firebase-init.js'; // Importa o firebase compat
 
 export default function Index() {
+  // Estado para armazenar o email e a senha
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
-  function handleEntrar(){
-    firebase.auth().signInWithEmailAndPassword("teste001@gmail.com", "123456")
+  // Função para lidar com o login
+  function handleEntrar() {
+    firebase.auth().signInWithEmailAndPassword(email, senha)
       .then(response => {
-        router.replace('/(tabs)/screens/')
+        router.replace('/(tabs)/screens/');
       })
       .catch(error => {
         console.error("Erro de login: ", error);
       });
   }
 
-  function handleCadastrar(){
-    router.navigate('/cadastro')
+  function handleCadastrar() {
+    router.navigate('/cadastro');
   }
 
   return (
     <View
-      style={[{
-        backgroundColor: COLORS.GRAY
-      }, styles.container]}
+      style={[{ backgroundColor: COLORS.GRAY }, styles.container]}
     >
-      <TextIcon isAbsolute={true} top={50}/>
-      <FormInput label="Email" placeholder="seu@email.com" />
-      <FormInput label="Senha" placeholder="senha" isPassword={true}/>
-      <BigButton title="Entrar" action={handleEntrar} type={1}/>
-      <BigButton title="Cadastrar" action={handleCadastrar} type={2}/>
+      <TextIcon isAbsolute={true} top={50} />
+      {/* Passamos o valor e a função para atualizar o estado */}
+      <FormInput 
+        label="Email" 
+        placeholder="seu@email.com" 
+        value={email} 
+        onChangeText={setEmail} 
+      />
+      <FormInput 
+        label="Senha" 
+        placeholder="senha" 
+        isPassword={true} 
+        value={senha} 
+        onChangeText={setSenha} 
+      />
+      <BigButton title="Entrar" action={handleEntrar} type={1} />
+      <BigButton title="Cadastrar" action={handleCadastrar} type={2} />
 
-      <Text
-          style={styles.forget}
-      >Esqueceu a senha?</Text>
+      <Text style={styles.forget}>
+        Esqueceu a senha?
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   forget: {
-      position: 'absolute',
-      bottom: '5%',
-      color: COLORS.ORANGE
+    position: 'absolute',
+    bottom: '5%',
+    color: COLORS.ORANGE,
   },
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
 });
--- src/components/formInput/index.tsx
+++ src/components/formInput/index.tsx
@@ -1,48 +1,50 @@
 import { COLORS } from '@/src/constants/Colors';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-interface FormInputProps{
-    label: string
-    placeholder: string
-    isPassword?: boolean
+interface FormInputProps {
+  label: string;
+  placeholder: string;
+  isPassword?: boolean;
+  value: string;
+  onChangeText: (text: string) => void;
 }
 
-export default function FormInput({label, placeholder, isPassword}: FormInputProps) {
- return (
-   <View 
-        className='flex justify-center items-center'
-        style={styles.container}
-   >
-        <Text 
-            style={styles.text}
-        >{label}</Text>
-        <TextInput
-            placeholder={placeholder}
-            placeholderTextColor={COLORS.GRAY}
-            secureTextEntry={isPassword}
-            style={styles.input}
-        />
-   </View>
+export function FormInput({ label, placeholder, isPassword, value, onChangeText }: FormInputProps) {
+  return (
+    <View 
+      className='flex justify-center items-center'
+      style={styles.container}
+    >
+      <Text style={styles.text}>{label}</Text>
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor={COLORS.GRAY}
+        secureTextEntry={isPassword}
+        style={styles.input}
+        value={value}
+        onChangeText={onChangeText} 
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: '80%'
-    },
-    text: {
-        alignSelf: 'flex-start',
-        fontSize: 16,
-        marginBottom: 10,
-        color: COLORS.WHITE
-    },
-    input: {
-        backgroundColor: COLORS.WHITE, 
-        padding: 15,
-        borderRadius: 15, 
-        fontSize: 16,
-        color: COLORS.GRAY,
-        width: '100%',
-        marginBottom: 10
-      },
-})
+  container: {
+    width: '80%',
+  },
+  text: {
+    alignSelf: 'flex-start',
+    fontSize: 16,
+    marginBottom: 10,
+    color: COLORS.WHITE,
+  },
+  input: {
+    backgroundColor: COLORS.WHITE,
+    padding: 15,
+    borderRadius: 15,
+    fontSize: 16,
+    color: COLORS.GRAY,
+    width: '100%',
+    marginBottom: 10,
+  },
+});
