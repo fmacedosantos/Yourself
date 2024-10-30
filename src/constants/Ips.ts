00766704b9f28ee36b5155b900118719a1d033d5
fmacedosantos <felipemacedo986@gmail.com>
2024-10-30 11:13:52 -0300

    Facilitando o esquema de rotas

--- src/constants/Ips.ts
+++
@@ -1,4 +0,0 @@
-export const IPS = {
-    SENAI: '192.168.56.1',
-    CASA: '192.168.17.21'
-}
--- src/constants/Routes.ts
+++ src/constants/Routes.ts
@@ -1,11 +1,11 @@
-import { IPS } from "./Ips";
+const IP = '192.168.0.229';
 
-export enum Paths {
+export enum PATHS {
     SHOW_ACTIVITIES = 'atividade/mostrar',
     SHOW_STATS = 'usuario/mostrar-estatisticas',
     REGISTER_USER = 'usuario/cadastrar'
 }
 
-export const ROUTES = (route: Paths) => {
-    return `http://${IPS.SENAI}:3000/${route}`;
+export const ROUTES = (route: PATHS) => {
+    return `http://${IP}:3000/${route}`;
 };
