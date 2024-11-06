c4fd881c1282d823dccd58c1c99fcc6d999b5e5e
fmacedosantos <felipemacedo986@gmail.com>
2024-11-06 13:21:57 -0300

    Adicionando o resumo das estatísitcas na loja

--- src/app/(tabs)/screens/shop/index.tsx
+++ src/app/(tabs)/screens/shop/index.tsx
@@ -1,11 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
 import { styles } from "./styles";
+import { useEffect, useState } from "react";
+import { userService } from "@/src/services/api/user";
+import { SummaryStats } from "@/src/components/summaryStats";
+
+interface ResumoEstatisticas {
+  ofensiva: number;
+  pontos: number;
+}
 
 export default function Shop() {
+  const [resumoEstatisticas, setResumoEstatisticas] = useState<ResumoEstatisticas>({
+    ofensiva: 0,
+    pontos: 0
+  });
+
+  useEffect(() => {
+    userService.carregarResumoEstatisticas(setResumoEstatisticas);
+  }, []);  
   return (
     <View
       style={styles.container}
     >
+      <SummaryStats 
+        ofensiva={resumoEstatisticas.ofensiva} 
+        pontos={resumoEstatisticas.pontos}
+      />
       <Text>Shop</Text>
     </View>
   );
