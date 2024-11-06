295f82a7b7c78b2d10c75578b91ba579f3d6646b
fmacedosantos <felipemacedo986@gmail.com>
2024-11-06 15:07:16 -0300

    Mudando o esquema da tela de login para adequar o 'Esqueceu a senha?'

--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -2,19 +2,20 @@ import { COLORS } from "@/src/constants/Colors";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    forget: {
-      position: 'absolute',
-      bottom: '5%',
-      fontSize: 16,
-      color: COLORS.ORANGE,
-      fontFamily: 'Itim-Regular',
-      textDecorationLine: 'underline' 
-    },
-    container: {
-      flex: 1,
-      backgroundColor: COLORS.GRAY,
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontFamily: 'Itim-Regular',
-    }
-  });
+  forget: {
+    fontSize: 16,
+    color: COLORS.ORANGE,
+    fontFamily: 'Itim-Regular',
+    textDecorationLine: 'underline',
+    textAlign: 'center', 
+    marginTop: 20, 
+  },
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.GRAY,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'Itim-Regular',
+    paddingBottom: 20, 
+  },
+});
