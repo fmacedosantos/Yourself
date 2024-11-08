9f7d27287acc6583c1ea9507a56713e16e3b5747
fmacedosantos <felipemacedo986@gmail.com>
2024-11-08 08:48:27 -0300

    Preparando a tela de configurações

--- src/app/(tabs)/settings/index.tsx
+++ src/app/(tabs)/settings/index.tsx
@@ -1,12 +1,41 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { styles } from './styles';
+import { useEffect, useState } from 'react';
+import { userService } from '@/src/services/api/user';
+import LoadingScreen from '@/src/components/loadindScreen';
+import { SummaryStats } from '@/src/components/summaryStats';
+
+interface ResumoEstatisticas {
+  ofensiva: number;
+  pontos: number;
+}
 
 export default function Settings() {
+  const [resumoEstatisticas, setResumoEstatisticas] = useState<ResumoEstatisticas>({
+    ofensiva: 0,
+    pontos: 0
+  });
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    async function carregarDados() {
+      userService.carregarResumoEstatisticas(setResumoEstatisticas);
+      setLoading(false)
+    }
+    carregarDados();
+  }, []);  
+
+  if (loading) {
+    return <LoadingScreen />; 
+  }
  return (
   <View
   style={styles.container}
->
-  <Text>Configurações</Text>
+  >
+    <SummaryStats 
+      ofensiva={resumoEstatisticas.ofensiva} 
+      pontos={resumoEstatisticas.pontos}
+    />
 </View>
   );
 }
