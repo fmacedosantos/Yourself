44396e640d97261d3cb2fedfe2e9b75e79b742c3
fmacedosantos <felipemacedo986@gmail.com>
2024-12-02 16:45:13 -0300

    Carregando os dados atualizados dos usuários na tela de perfil

--- src/app/(tabs)/screens/profile/index.tsx
+++ src/app/(tabs)/screens/profile/index.tsx
@@ -1,7 +1,8 @@
 import { Text, View } from "react-native";
 import { styles } from "./styles";
 import { Link, router } from "expo-router";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/core";
 import { SummaryStats } from "@/src/components/summaryStats";
 import { SolidButton } from "@/src/components/solidButton";
 import { BestStats } from "@/src/components/bestStats";
@@ -14,9 +15,9 @@ interface ResumoEstatisticas {
 }
 
 interface Informacoes {
-  nome: string
-  apelido: string
-  anoRegistro: number
+  nome: string;
+  apelido: string;
+  anoRegistro: number;
 }
 
 interface MelhoresEstatisticas {
@@ -27,59 +28,67 @@ interface MelhoresEstatisticas {
 export default function Profile() {
   const [resumoEstatisticas, setResumoEstatisticas] = useState<ResumoEstatisticas>({
     ofensiva: 0,
-    pontos: 0
+    pontos: 0,
   });
   const [informacoes, setInformacoes] = useState<Informacoes>({
-    nome: '',
-    apelido: '',
-    anoRegistro: 0
+    nome: "",
+    apelido: "",
+    anoRegistro: 0,
   });
   const [melhoresEstatisticas, setMelhoresEstatisticas] = useState<MelhoresEstatisticas>({
     maiorOfensiva: 0,
-    totalPontos: 0
+    totalPontos: 0,
   });
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function carregarDados() {
-       await carregarResumoEstatisticas(setResumoEstatisticas);
-       await carregarUsuario(setInformacoes);
-       await carregarMelhoresEstatisticas(setMelhoresEstatisticas);
-      setLoading(false);
-    }
-    carregarDados();
-  }, []);  
+  const carregarDados = useCallback(async () => {
+    setLoading(true);
+    await carregarResumoEstatisticas(setResumoEstatisticas);
+    await carregarUsuario(setInformacoes);
+    await carregarMelhoresEstatisticas(setMelhoresEstatisticas);
+    setLoading(false);
+  }, []);
 
-  function handleGoToSettings(){
-    router.navigate('/(tabs)/confirmPassword');
+  useFocusEffect(
+    useCallback(() => {
+      carregarDados();
+    }, [carregarDados])
+  );
+
+  function handleGoToSettings() {
+    router.navigate("/(tabs)/confirmPassword");
   }
 
-  async function handleLeaveAccount(){
+  async function handleLeaveAccount() {
     await logout();
   }
 
   if (loading) {
-    return <LoadingScreen />; 
+    return <LoadingScreen />;
   }
-
 
   return (
     <View style={styles.container}>
-      <SummaryStats 
-        ofensiva={resumoEstatisticas.ofensiva} 
-        pontos={resumoEstatisticas.pontos}
-      />
+      <SummaryStats ofensiva={resumoEstatisticas.ofensiva} pontos={resumoEstatisticas.pontos} />
       <View style={styles.containerInformacoes}>
         <Text style={styles.nome}>{informacoes.nome}</Text>
         <Text style={styles.apelido}>{informacoes.apelido}</Text>
         <Text style={styles.anoRegistro}>Na plataforma desde {informacoes.anoRegistro}</Text>
       </View>
       <View style={styles.accountContainer}>
-        <SolidButton title="Configurações" action={handleGoToSettings} style={styles.settingsButton}/>
-        <SolidButton title="Sair da conta" action={handleLeaveAccount} style={styles.leaveAccountButton}/>
+        <SolidButton
+          title="Configurações"
+          action={handleGoToSettings}
+          style={styles.settingsButton}
+        />
+        <SolidButton
+          title="Sair da conta"
+          action={handleLeaveAccount}
+          style={styles.leaveAccountButton}
+        />
       </View>
 
-      <BestStats melhorOfensiva={melhoresEstatisticas.maiorOfensiva} totalXp={melhoresEstatisticas.totalPontos}/>
+      <BestStats melhorOfensiva={melhoresEstatisticas.maiorOfensiva} totalXp={melhoresEstatisticas.totalPontos} />
     </View>
   );
 }
