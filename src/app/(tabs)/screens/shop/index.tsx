b98866ff0744c5dfc7bcfe5639f3d5285ea3bf08
fmacedosantos <felipemacedo986@gmail.com>
2024-12-02 22:43:18 -0300

    Montando a loja com itens padrões

--- src/app/(tabs)/screens/shop/index.tsx
+++ src/app/(tabs)/screens/shop/index.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { styles } from "./styles";
 import { useEffect, useState } from "react";
 import { SummaryStats } from "@/src/components/summaryStats";
 import LoadingScreen from "@/src/components/loadindScreen";
 import { carregarResumoEstatisticas } from "@/src/services/api/user";
 import { Title } from "@/src/components/title";
+import { Item } from "@/src/components/item";
 
 interface ResumoEstatisticas {
   ofensiva: number;
@@ -14,32 +15,42 @@ interface ResumoEstatisticas {
 export default function Shop() {
   const [resumoEstatisticas, setResumoEstatisticas] = useState<ResumoEstatisticas>({
     ofensiva: 0,
-    pontos: 0
+    pontos: 0,
   });
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function carregarDados() {
       await carregarResumoEstatisticas(setResumoEstatisticas);
-      setLoading(false)
+      setLoading(false);
     }
     carregarDados();
-  }, []);  
+  }, []);
 
   if (loading) {
-    return <LoadingScreen />; 
+    return <LoadingScreen />;
   }
+
   return (
-    <View
-      style={styles.container}
-    >
-      <SummaryStats 
-        ofensiva={resumoEstatisticas.ofensiva} 
-        pontos={resumoEstatisticas.pontos}
-      />
-      <Title title="Loja de itens" containerStyle={{ position: 'absolute', top: 0 }} />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <SummaryStats
+          ofensiva={resumoEstatisticas.ofensiva}
+          pontos={resumoEstatisticas.pontos}
+        />
+        <Title
+          title="Loja de itens"
+        />
+      </View>
+
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Item icon={"https://i.imgur.com/xDvCgMu.png"} name="Laranja" price={500} />
+        <Item icon={"https://i.imgur.com/xDvCgMu.png"} name="Laranja" price={500} />
+        <Item icon={"https://i.imgur.com/xDvCgMu.png"} name="Laranja" price={500} />
+        <Item icon={"https://i.imgur.com/xDvCgMu.png"} name="Laranja" price={500} />
+        <Item icon={"https://i.imgur.com/xDvCgMu.png"} name="Laranja" price={500} />
+        <Item icon={"https://i.imgur.com/xDvCgMu.png"} name="Laranja" price={500} />
+      </ScrollView>
     </View>
   );
 }
-
-
--- src/app/(tabs)/screens/shop/styles.ts
+++ src/app/(tabs)/screens/shop/styles.ts
@@ -2,10 +2,25 @@ import { COLORS } from "@/src/constants/Colors";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: COLORS.GRAY,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
-  })
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.GRAY,
+  },
+  header: {
+    alignItems: "center",
+    justifyContent: "space-between", 
+    paddingHorizontal: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: COLORS.GRAY, 
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 10,
+    justifyContent: "center", 
+    paddingHorizontal: 20,
+    paddingTop: 10, 
+  },
+});
